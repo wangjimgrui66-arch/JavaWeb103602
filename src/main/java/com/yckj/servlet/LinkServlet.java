3ffefbb7a426773f0322b1f97c055ffc22f52d71
wangjimgrui6-arch <wangjimgrui66@gmail.com>
2026-04-25 10:16:57 +0800

    更改

---
+++ src/main/java/com/yckj/servlet/LinkServlet.java
@@ -0,0 +1,36 @@
+package com.yckj.servlet;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author wjr
+ * @version 1.0
+ */
+@WebServlet("/LinkServlet")
+public class LinkServlet extends HttpServlet {
+    // 处理第一个请求之前 被容器（如 Tomcat）自动调用；只执行1次
+    public void init() {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
+            IOException {
+        String uname = request.getParameter("username");
+        String nation = request.getParameter("nation");
+        String gender = request.getParameter("gender");
+        System.out.println(gender+"=======");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
+            IOException {
+        this.doGet(request, response);
+    }
+}
+ 
